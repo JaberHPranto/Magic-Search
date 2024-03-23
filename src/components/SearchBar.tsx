5040c1d69cfb6aebec5b824ae5ad62b4289c492f
JaberHPranto <jabearhossain@iut-dhaka.edu>
2024-03-23 17:35:15 +0600

    complete searchBar UI

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useRef, useState, useTransition } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Loader2, Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+const SearchBar = () => {
+  const [query, setQuery] = useState("");
+  const [isSearching, startTransition] = useTransition();
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  const handleSearch = () => {
+    startTransition(() => {
+      router.push(`/search?query=${query}`);
+    });
+  };
+
+  const handleFocus = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    } else if (e.key === "Escape") {
+      inputRef?.current?.blur();
+    }
+  };
+
+  return (
+    <div className="relative w-full h-14 flex flex-col bg-white">
+      <div className="relative h-14 z-10 rounded-md">
+        <Input
+          ref={inputRef}
+          onKeyDown={handleFocus}
+          disabled={isSearching}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="absolute inset-0 h-full text-lg"
+        />
+        <Button
+          className="absolute right-0 inset-y-0 h-full rounded-lg"
+          disabled={isSearching}
+          onClick={handleSearch}
+        >
+          {isSearching ? (
+            <Loader2 className="w-6 h-6 animate-spin" />
+          ) : (
+            <Search />
+          )}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
